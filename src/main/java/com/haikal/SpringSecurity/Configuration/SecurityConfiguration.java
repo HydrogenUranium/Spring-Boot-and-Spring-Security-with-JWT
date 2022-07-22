085c05a0282dbb4005b6067fc621a21277179013
Kadar <haikal.kadar@lr.org>
2022-07-22 19:24:04 +0800

    Update security configuration

--- src/main/java/com/haikal/SpringSecurity/Configuration/SecurityConfiguration.java
+++ src/main/java/com/haikal/SpringSecurity/Configuration/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package com.haikal.SpringSecurity.Configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-            .ignoring()
-            .antMatchers("/h2-console/**");
+public class SecurityConfiguration {
+    /*
+     * @Override
+     * public void configure(WebSecurity web) throws Exception {
+     * web
+     * .ignoring()
+     * .antMatchers("/h2-console/**");
+     * }
+     */
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**", "/ignore2");
     }
 }
