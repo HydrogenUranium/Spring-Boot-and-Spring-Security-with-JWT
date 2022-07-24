b53809d18093c166824250e73ab01ff2d73b5b3f
Kadar <hydrogen.uranium238@gmail.com>
2022-07-24 15:34:13 +0800

    update user controller

--- src/main/java/com/haikal/SpringSecurity/Controller/UserController.java
+++ src/main/java/com/haikal/SpringSecurity/Controller/UserController.java
@@ -1,14 +1,18 @@
 package com.haikal.SpringSecurity.Controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.haikal.SpringSecurity.Domain.Role;
 import com.haikal.SpringSecurity.Domain.User;
 import com.haikal.SpringSecurity.Service.UserService;
 
@@ -25,10 +29,24 @@ public class UserController {
     @GetMapping("/user")
     public ResponseEntity<List<User>> getAllUsers() {
         log.info("User controller get all users ");
-        log.info("User controller get all users ");
-        log.info("User controller get all users ");
+
         return ResponseEntity.ok().body(userService.getListUser());
 
+    }
+
+    /*
+     * Used URI with created respond to indicate resource has been created in server
+     */
+    @PostMapping("user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
     }
 
 }
